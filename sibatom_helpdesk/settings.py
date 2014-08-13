2741db277cda0ce3edad236a6b0bc36b697cd4ce
drozdek <samret13@gmail.com>
2014-08-13 10:36:36 +0900

    Небольшие косметические изменения.

--- sibatom_helpdesk/settings.py
+++ sibatom_helpdesk/settings.py
@@ -4,7 +4,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('drozdek', 'samret13@gmail.com'),
 )
 
 EMAIL_HOST = 'mail.sibatom.com'
@@ -24,13 +24,11 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'sibhelpdesk',  # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
+        'NAME': 'sibhelpdesk',
         'USER': 'admin',
         'PASSWORD': 'sibhelp',
-        'HOST': '',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',  # Set to empty string for default.
+        'HOST': '',
+        'PORT': '',
     }
 }
 
