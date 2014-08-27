5107c58a21313d1bc907dc2f3ad32df455ce44f0
drozdek <samret13@gmail.com>
2014-08-27 09:15:30 +0900

    Перестроил структуру отправки талона с публичной страницы. Произвёл рефактор HTML кода.

--- sibatom_helpdesk/urls.py
+++ sibatom_helpdesk/urls.py
@@ -2,17 +2,18 @@ from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'sibatom_helpdesk.views.home', name='home'),
-    # url(r'^sibatom_helpdesk/', include('sibatom_helpdesk.foo.urls')),
+                       # Examples:
+                       # url(r'^$', 'sibatom_helpdesk.views.home', name='home'),
+                       # url(r'^sibatom_helpdesk/', include('sibatom_helpdesk.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       # Uncomment the admin/doc line below to enable admin documentation:
+                       url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('helpdesk.urls')),
+                       # Uncomment the next line to enable the admin:
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'', include('helpdesk.urls')),
 )
